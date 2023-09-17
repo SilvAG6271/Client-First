09e662523c2a4b6da2d1077ce116d5160004d045
auron <auron7985@gmail.com>
2023-09-17 17:54:01 -0500

    creating on Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,8 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({jate: content});
-  const result = await request;
+  const result = await store.put({jate: content});
   console.log('data saved to the database', result);
   console.error('putDb not implemented');
 }
@@ -31,8 +30,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
+  const result = await store.get(1);
   console.log('result.value', result);
   console.error('getDb not implemented');
   return result.value;
